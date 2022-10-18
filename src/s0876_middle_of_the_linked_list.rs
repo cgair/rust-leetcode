a0c2b4c77c1677be72376812689149ea7fc50603
cgair <chengeair23@sjtu.edu.cn>
2022-10-18 17:46:45 +0800

    Add 876 Middle of the Linked List and rename 142 file's name

--- src/s0876_middle_of_the_linked_list.rs
+++ src/s0876_middle_of_the_linked_list.rs
@@ -1,3 +1,20 @@
+/**
+ * [1] Two Sum
+ *
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ * If there are two middle nodes, return the second middle node.
+ * 
+ * Example:
+ *
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [3,4,5]
+ * Explanation: The middle node of the list is node 3.
+ * 
+ * Input: head = [1,2,3,4,5,6]
+ * Output: [4,5,6]
+ * Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ */
 // problem: https://leetcode.cn/problems/middle-of-the-linked-list/
 use crate::{ListNode, to_list};
 

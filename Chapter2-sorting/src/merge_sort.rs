f759b2d77ef8f9ac0774aa195eb1a95aba2098ca
cgair <chengeair23@sjtu.edu.cn>
2023-02-16 17:03:43 +0800

    Add C style merge sort

--- Chapter2-sorting/src/merge_sort.rs
+++ Chapter2-sorting/src/merge_sort.rs
@@ -18,15 +18,16 @@
 /// Rearranges the array in ascending order, using
 /// [merge sort](https://en.wikipedia.org/wiki/Merge_sort).
 /// 
-pub fn merge_sort<T: Copy>(arr: &mut [T]) {
+pub fn merge_sort<T: Copy + PartialOrd + Default>(arr: &mut [T]) {
     let len = arr.len();
-    let mut aux = Vec::with_capacity(len);
+    // let mut aux = Vec::with_capacity(len);
+    let mut aux = vec![T::default(); len];
     sort(arr, &mut aux, 0, len - 1);
 
 }
 
 /// Mergesort a[lo..hi] using auxiliary array aux[lo..=hi]
-fn sort<T: Copy>(arr: &mut [T], aux: &mut [T], lo: usize, hi: usize) {
+fn sort<T: Copy + PartialOrd>(arr: &mut [T], aux: &mut [T], lo: usize, hi: usize) {
     if lo >= hi { return; }
 
     let mid = (lo + hi) / 2;
@@ -37,24 +38,58 @@ fn sort<T: Copy>(arr: &mut [T], aux: &mut [T], lo: usize, hi: usize) {
 }
 
 /// Stably merge a[lo .. mid] with a[mid+1 ..hi] using aux[lo .. hi]
-fn merge<T: Copy>(arr: &mut [T], aux: &mut [T], lo: usize, mid: usize, hi: usize) {
+fn merge<T: Copy + PartialOrd>(arr: &mut [T], aux: &mut [T], lo: usize, mid: usize, hi: usize) {
     // precondition: a[lo .. mid] and a[mid + 1 .. hi] are sorted subarrays
 
-    // copy to aux[]
-    for i in lo..=hi {
-        aux[i] = arr[i];
+    /* This is more C*/
+    let mut i = lo;  // 第一个数组索引, 指向第一个元素.
+    let mut j = mid + 1;  // 第二个数组索引, 指向第一个元素.
+
+    for t in 0..=hi-lo {    // 临时数组索引, 指向第一个元素.
+        // 若 i 到达第一个数组的尾部, 将第二个数组余下元素复制到 临时数组中
+        if i == mid + 1 {
+           aux[t] = arr[j];
+           j += 1;
+           continue;
+        }
+        // 若 j 到达第二个数组的尾部, 将第一个数组余下元素复制到 临时数组中
+        if j == hi + 1 {
+            aux[t] = arr[i];
+            i += 1;
+            continue;
+        }
+        // 如果第一个数组的当前元素 比 第二个数组的当前元素小,将 第一个数组的当前元素复制到 临时数组中
+        if arr[i] < arr[j] {
+            aux[t] = arr[i];
+            i += 1
+        } else {
+            aux[t] = arr[j];
+            j += 1;
+        }
     }
 
-    // merge back to a[]
-
+    // merge back to arr[]
+    // i = lo (被排序的数组 arr 的起始位置)
+    // j = 0 (临时数组的起始位置)
+    arr[lo..=hi].copy_from_slice(&aux[0..=hi - lo]);
 
     // postcondition: a[lo .. hi] is sorted
 }
 
 #[test]
 fn test_merge_sort() {
-    let mut value = vec![5, 1, 9, 3, 7, 4, 8, 6, 2];
-    merge_sort(&mut value);
+    println!("Sort numbers ascending");
+    let mut numbers = [4, 65, 2, -31, 0, 99, 2, 83, 782, 1];
+    println!("Before: {:?}", numbers);
+    merge_sort(&mut numbers);
+    println!("After:  {:?}\n", numbers);
+    assert_eq!(numbers, [-31, 0, 1, 2, 2, 4, 65, 83, 99, 782]);
+
+    let mut numbers = vec![5, 1, 9, 3, 7, 4, 8, 6, 2];
+    println!("Before: {:?}", numbers);
+    merge_sort(&mut numbers);
+    println!("After:  {:?}\n", numbers);
+    assert_eq!(numbers, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 }
 
 
